62dc24677d80d3541ef679ca5bfe5d8f77e37a07
j1nu <gle00@naver.com>
2020-09-06 01:35:29 +0900

    test6

--- rximagepicker_support_zhihu/src/main/java/com/qingmei2/rximagepicker_extension_zhihu/ui/ZhihuImagePickerFragment.kt
+++ rximagepicker_support_zhihu/src/main/java/com/qingmei2/rximagepicker_extension_zhihu/ui/ZhihuImagePickerFragment.kt
@@ -210,7 +210,8 @@ class ZhihuImagePickerFragment : androidx.fragment.app.Fragment(), IGalleryCusto
             childFragmentManager
                     .beginTransaction()
                     .replace(R.id.container, fragment, tag)
-                    .commitNow()
+                    .commitNowAllowingStateLoss()
+//                    .commitNow()
 //                    .commitAllowingStateLoss()
         } else {
             println("ascassaasac")
